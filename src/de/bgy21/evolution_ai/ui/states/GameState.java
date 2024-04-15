1aed932d7fc491eb68079129a8a6c1ab7f8a544a
LaptopmanMann <florianagario2@gmail.com>
2024-04-15 08:38:42 +0200

    Hab die Imports für Lousiana gefixt

--- src/de/bgy21/evolution_ai/ui/states/GameState.java
+++ src/de/bgy21/evolution_ai/ui/states/GameState.java
@@ -4,6 +4,7 @@ import de.bgy21.evolution_ai.ui.world.Block;
 import de.bgy21.evolution_ai.ui.world.Grid;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
