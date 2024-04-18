30603abd7ac2254b62cee4de4a1b7ea9898b9f03
UwUyourmom <louis02102005@gmail.com>
2024-04-18 18:40:40 +0200

    Esc inplemented in flappydings

--- src/de/bgy21/evolution_ai/ui/screens/flappybird/GameScreen.java
+++ src/de/bgy21/evolution_ai/ui/screens/flappybird/GameScreen.java
@@ -2,6 +2,7 @@ package de.bgy21.evolution_ai.ui.screens.flappybird;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -27,6 +28,9 @@ public class GameScreen extends BasicGameState {
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-
+        Input input = gameContainer.getInput();
+        if(input.isKeyPressed(Input.KEY_ESCAPE)){
+            gameContainer.exit();
+        }
     }
 }
